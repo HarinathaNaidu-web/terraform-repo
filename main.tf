1b59264eff3267fdb10c3e7e33048d06c934a7bb
EC2 Default User <ec2-user@ip-172-31-36-124.us-west-2.compute.internal>
2026-04-03 17:13:31 +0000

    updated main file for pipeline

--- main.tf
+++ main.tf
@@ -1,8 +1,19 @@
-provider "aws" {
-  region = "us-west-2"
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-x86_64"]
+  }
 }
 
 resource "aws_instance" "test" {
-  ami           = "ami-0c55b159cbfafe1f0"
+  ami           = data.aws_ami.amazon_linux.id
   instance_type = "t2.micro"
+
+  tags = {
+    Name = "test-instance"
+  }
 }
