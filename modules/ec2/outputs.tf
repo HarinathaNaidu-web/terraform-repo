0ac190aec88c2d5b855ff9e1d3204e8a975e580f
Nam-repo <harinatha.naidu@nam-it.com>
2026-04-06 21:51:17 +0530

    Update outputs.tf

--- modules/ec2/outputs.tf
+++ modules/ec2/outputs.tf
@@ -1,3 +1,8 @@
-output "instance_id" {
-  value = aws_instance.this.id
+resource "aws_instance" "this" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "Terraform-EC2"
+  }
 }
