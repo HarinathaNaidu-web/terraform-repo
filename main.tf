33d3de046e9b5c84ff4c94dbd1da1ee93d2ff684
harinatha naidu <harinatha.naidu@nam-it.com>
2026-04-06 14:17:37 +0000

    NewFormat-added

--- main.tf
+++ main.tf
@@ -1,17 +1,6 @@
 
-data "aws_ami" "amazon_linux" {
-  most_recent = true
-  owners      = ["amazon"]
-
-  filter {
-    name   = "name"
-   # values = ["al2023-ami-*-x86_64"]
-    values = ["ami-0f58b397bc5c1f2e8"]
-  }
-}
-
 resource "aws_instance" "test" {
-  ami           = data.aws_ami.amazon_linux.id
+  ami           = "ami-0c2d3e23c2b8d84f0"  # Amazon Linux 2023 - us-west-2
   instance_type = "t2.micro"
 
   tags = {
