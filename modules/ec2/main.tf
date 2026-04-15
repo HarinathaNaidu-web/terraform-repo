f41233bd0ff186770b1b812709e79553a4e8bbdd
Nam-repo <harinatha.naidu@nam-it.com>
2026-04-15 14:08:19 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -3,8 +3,15 @@ resource "aws_instance" "this" {
   instance_type = var.instance_type
 
   subnet_id = var.subnet_id
-
   vpc_security_group_ids = [var.security_group_id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "ec2-user:MyStrongPassword123" | chpasswd
+              sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+              sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
+              systemctl restart sshd
+              EOF
 
   tags = {
     Name = "Terraform-EC2"
