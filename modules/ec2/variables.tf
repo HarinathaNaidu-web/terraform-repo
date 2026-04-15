9182303604b5c4de9785bf38f4d92df2582e1198
Nam-repo <harinatha.naidu@nam-it.com>
2026-04-15 14:20:11 +0530

    Update variables.tf

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -5,3 +5,10 @@ variable "ami_id" {
 variable "instance_type" {
   type = string
 }
+variable "subnet_id" {
+  description = "Subnet ID"
+}
+
+variable "security_group_id" {
+  description = "Security Group ID"
+}
